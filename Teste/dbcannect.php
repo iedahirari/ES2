400a33af8ffc713a1c75e56cde82016009b53f3b
Thais <59621122+Thais-Fer@users.noreply.github.com>
2020-05-23 22:19:00 -0300

    TESTE QUE DEU CERTO!!!!!!!!!!!! CRAWLER

--- Teste/dbcannect.php
+++ Teste/dbcannect.php
@@ -1,12 +1,19 @@
 <?php
 
 //ENTER YOUR DATABASE CONNECTION INFO BELOW:
-  $hostname="localhost";
-  $database="teste";
-  $username="root";
-  $password="";
+  
 
-//DO NOT EDIT BELOW THIS LINE
-$link = mysqli_connect($hostname, $username, $password);
-mysqli_select_db($link,$database) or die('Could not select database');
+	function connection(){
+		$hostname="localhost";
+		$database="bdmonitorae";
+		$username="root";
+		$password="";
+		$link = mysqli_connect($hostname, $username, $password,$database);
+		return $link;
+
+	}
+
+	function sair($link){
+		mysqli_close($link);
+	}
 ?>
--- Teste/vai_funcionar.php
+++ Teste/vai_funcionar.php
@@ -1,24 +1,17 @@
 <?php
 
-require_once('dbcannect.php'); //cAnnect AMIGA
+		require_once('dbcannect.php'); //cAnnect AMIGA
+		$connection = connection();
+		$json =  file_get_contents('http://www.licitacao.pmmc.com.br/Transparencia/vencimentos2');
+        $jsonTratado = iconv('UTF-8', 'UTF-8//IGNORE', $json);
+        $objVencimentos=json_decode(stripslashes($jsonTratado));
+       	foreach ($objVencimentos->servidores as $servidor) {
+       	echo $servidor->nome.'<br>';
 
-$json_file = file_get_contents('servidores.json');   
-$json_str = json_decode($json_file);
-$itens = $json_str['nodes'];
-
-// processing the array of objects
-foreach ($itens as $dados) {
-    $nome = $dados['nome'];
-    $cargo = $dados['cargo'];
-    $rendimento = $dados['rendimento'];
-
-
+       	
     // preparing statement for insert query
-    $st = mysqli_prepare($connection, 'INSERT INTO dados(nome, cargo, rendimento) VALUES (?, ?, ?)');
+    	$st = mysqli_query($connection, "INSERT INTO tb_funcionario(rgf, nome , cargo, rendimentos) VALUES ( '".$servidor->rgf."','". $servidor->nome."','". $servidor->cargo."','". $servidor->rendimentos."')");
 
     // bind variables to insert query params
-    mysqli_stmt_bind_param($st, 'ssss', $nome, $cargo, $rendimento);
-
-    // executing insert query
-    mysqli_stmt_execute($st);
-}
+    	echo $st;
+} sair($connection);
